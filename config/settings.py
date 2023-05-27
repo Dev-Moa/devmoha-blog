fa5812a4dd5d2bff841868dd26f34afaefd417cc
unknown <codewithmoha@gmail.com>
2023-05-27 23:09:21 +0300

    csrf origin

--- config/settings.py
+++ config/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['devmoha-blog-production.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://devmoha-blog-production.up.railway.app']
 
 # Application definition
 
